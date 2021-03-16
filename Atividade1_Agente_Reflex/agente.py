11437eb0c4ae807fa138e0e1647eeb3a8c7d9956
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-03-15 23:23:27 -0300

    adding commentary

--- Atividade1_Agente_Reflex/agente.py
+++ Atividade1_Agente_Reflex/agente.py
@@ -1,12 +1,21 @@
 import random
 
 class Ambiente:
+    """
+        Classe responsável por manter armazenar
+        o estado atual do ambiente em que o agente está
+        inserido.
+    """
     def __init__(self):
-        self.posicao_atual = "A" if random.randint(0,1) else "B"
-        self.situacao_atual = None 
-        self.estavel = 0
+        self.posicao_atual = "A" if random.randint(0,1) else "B" # posicao atual do aspirador de pó
+        self.situacao_atual = None # armazena a ultima leitura do sensor
+        self.estavel = 0 # soma a quantidade de estados limpos seguidos para evitar se movimentar desnecessariamente
 
     def definir_situacao(self, nova_situacao):
+        """
+            checa se a situacao atual é igual ou diferente da acao anterior
+            e atualiza a variavel que calcula as rodadas estaveis
+        """
         if self.situacao_atual == nova_situacao and nova_situacao != "Sujo":
             self.estavel += 1
         else:
@@ -14,19 +23,30 @@ class Ambiente:
             self.situacao_atual = nova_situacao
     
     def ambiente_estavel(self):
+        """
+            Retorna se o ambiente é estável ou não 
+        """
         if self.estavel>=2:
             return True
         else:
             return False
 
 class Percepcao:
+    """
+        Classe responsável por simular a ação de um sensor.
+        Verifica qual o estado do ambiente através da entrada do usuário
+    """
     def __init__(self):
-        self.situacao = None 
+        self.situacao = None # situação do ambiente
 
     def recuperar_situacao(self):
         return self.situacao
 
     def verificar_ambiente(self):
+        """
+            Se o usuário digitar o número 0, o sensor entenderá o ambiente como sujo.
+            Do contrario, o ambiente será entendido como limpo
+        """
         entrada_sensor = int(input("Estado do Ambiente (0-Sujo,1-Limpo):"))
         if entrada_sensor == 0:
             self.situacao = "Sujo"
@@ -36,6 +56,12 @@ class Percepcao:
         return self.situacao
 
 class Agente:
+    """
+        Classe responsável por representar o agente,
+        detem uma instancia do sensor e do ambiente.
+        Determina qual a ação a ser tomada de acordo com o estado do sensor e do ambiente.
+        Retorna ao usuário a ação escolhida 
+    """
     def __init__(self):
         self.sensor =  Percepcao()
         self.ambiente = Ambiente()
